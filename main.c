b3e76a97aa3e89dfb08f43cd1906397757be01ab
PasztorGergo <pasztorg05@gmail.com>
2023-11-05 07:53:04 +0100

    memory-leak-fix

--- main.c
+++ main.c
@@ -2,20 +2,46 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include "../../../../../Downloads/debugmalloc.h"
 
-typedef struct main
+typedef struct
 {
     char character;
-    char *morese;
+    char *morse;
 } kod;
 
+char* hosszu_sort_olvas(){
+    char *tomb;
+    int meret = 1;
+    char c = '0';
+    tomb = (char*) malloc(meret * sizeof(char));
+    scanf("%c", &c);
 
-void KeszletBe(char *path);
+    while(scanf("%c", &c) != EOF){
+        meret++;
+        if(c == '\n')
+            break;
 
-void MorzeToText(char *source, char *dest);
+        char temp[meret] = {};
 
-void TextToMorze(char *source, char *dest);
+        for(int i = 0; i < meret-2; i++){
+            temp[i] = tomb[i];
+        }
 
+        free(tomb);
+        tomb = (char*) malloc(meret * sizeof(char));
+        
+        for(int i = 0; i < meret-2; i++){
+            tomb[i] = temp[i];
+        }
+        tomb[meret-2] = c;
+        if(meret > 2) tomb[meret-1] = '\0';
+    }
+
+    return tomb;
+}
+char* Convert(char* source, char* input, bool isTextToMorse);
+kod* ReadCharset(char *path);
 /*
         Todo:
         - Text to morse converter
@@ -26,84 +52,94 @@ void TextToMorze(char *source, char *dest);
         - When choosing the input option clear the console for better visibility
     */
 int main()
-{   
-    int action;
+{
     bool isTextToMorse = false;
-    char *szoveg;
-    char *cel;
-    FILE *fp;
-    int i = 0;
-
-
-
+    int action;
+    char *input;
+    char *path;
+    kod *abc = NULL;
 
     printf("			___\n_-||-_		       \\   /\n|----|_--___---__I_#_--|   |\n|    |__I____|*|_______|___/\n|----|  |*|   @       |\n|_______|-|__________/\n/_______________#____\\\n\n\n");
     printf("Morze kodolo %s\nValasszon milyen muveletet szeretne vegrehajtani es irja le a sorszamat\n0. Forditas iranyanak megvaltoztatasa\n1. Bemenet megadasa\n2. Karakterkeszlet megadasa\n3. Kilépés a programból\n", isTextToMorse ? "(Szöveg -> Morze)" : "(Morze -> Szöveg)");
-    
+
     while(scanf("%d", &action) != EOF){
-        switch (action){
+        switch (action)
+        {
         case 0:
             isTextToMorse = !isTextToMorse;
-            printf("\nA fordítás iránya megváltoztatva a következőre: %s\n", isTextToMorse ? "Szöveg -> Morze" : "Morze -> Szöveg");
+            printf("A forditas iranya megvaltozott: %s", isTextToMorse ? "Szoveg -> Morze" : "Morze -> Szoveg");
             break;
-        
         case 1:
-            printf("Adja meg a %s kívánt üzenetet\n", isTextToMorse ? "kódolni" : "dekódolni");
-            scanf("%s", szoveg);
-            if(isTextToMorse)
-                TextToMorze(szoveg, cel);
-            else
-                MorzeToText(szoveg, cel);
-            printf("A %s üzenet a következő\n%s", isTextToMorse ? "kódólt":"visszafejtett", *cel);
+            printf("Adja meg a %s szoveget:\n", isTextToMorse ? "kodolando" : "dekodolando");
+            input = hosszu_sort_olvas();
+            printf("\nA bemenet:%s\n", input);
+            free(input);
             break;
-
         case 2:
-            char *path;
-            char sor[10];
-            printf("Adja meg a karakterkészlet elérhetőségét\n");
-            scanf("%s", path);
-            fp = fopen(path, "r");
-            //Reads all the lines of a txt file
-            while(fgets(sor, 10, fp)){
-                i++;
-            }
-
-            kod abc[i] = {};
-
-            char be[10];
-            while(i > 0){
-                abc[i].character = be[0];
-                i--;
-            }
-            fclose(fp);
+            path = hosszu_sort_olvas();
+            if(abc != NULL)
+                free(abc);
+            
+            abc = ReadCharset(path);
+            free(path);
             break;
-        
         case 3:
+            free(abc);
             exit(1);
             break;
         
         default:
-            printf("A művelet nem létezik. Adja meg a művelet sorszámát a listáról!\n");
             break;
         }
         printf("Morze kodolo %s\nValasszon milyen muveletet szeretne vegrehajtani es irja le a sorszamat\n0. Forditas iranyanak megvaltoztatasa\n1. Bemenet megadasa\n2. Karakterkeszlet megadasa\n3. Kilépés a programból\n", isTextToMorse ? "(Szöveg -> Morze)" : "(Morze -> Szöveg)");
     }
-
     return 0;
 }
 
-void TextToMorze(char *source, char *dest){
+kod* ReadCharset(char *path){
+    FILE* fp;
+    kod *alphabet;
+    int meret = 0;
+    alphabet = (kod*) malloc(meret * sizeof(kod));
+    char sor[101];
+
+    fp = fopen(path,"r");
+    while(fgets(sor,101,fp)){
+        if(sor != ""){
+            printf("%s", sor);
+            if(meret > 0){
+                kod temp[meret] = {};
+
+                for(int i = 0; i < meret-1; i++){
+                    temp[i] = alphabet[i];
+                }
+
+                free(alphabet);
+                alphabet = (kod*) malloc(meret * sizeof(kod));
+
+                for(int i = 0; i < meret; i++){
+                    alphabet[i] = temp[i];
+                }
+
+                sscanf(sor,"%c\t%s", alphabet[meret - 1].character, alphabet[meret - 1].morse);
+                //printf("%c\t%s", alphabet[meret - 1].character, alphabet[meret - 1].morse);
+            }
+            meret++;
+        }
+    }
+    fclose(fp);
+    return alphabet;
+}
+char* Convert(char *source, char *input, bool isTextToMorse){
+    if(isTextToMorse){
+
+    }else{
+
+    }
     //Replace chars from the beginning with their morse counterpart
         //Using two arrays for the chars and search for their encoded version
         //Creating an array of a custom type containing the letter and the coded form
     //Place the newly generated morse code to the destination pointer
-};
-
-void MorzeToText(char *source, char *dest){
     //Replace morse code from the beginning with their alphabetic counterpart
     //Place the newly generated text to the destination pointer
-}
-
-void KeszletBe(char *path){
-    //Read the file at the given path and use this encoding until changed.
 }
